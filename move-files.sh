f06fb509c5eb8daaacc48f71002b8d1683b914dd
evelyndrake <evelyndrake90@gmail.com>
2025-03-24 12:46:21 -0400

    Edited install script

--- move-files.sh
+++ move-files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 destination="../cgi-bin"
 cp -r * "$destination/"
-#Add /~jcd171/ to all img tags
-find "$destination" -type f -name "*.php" -exec sed -i 's/src=\'icons\//src=\'\/~jcd171\/icons\//g' {} +
+#Add /~jcd171/ to all img tags (src='icons/')
+find "$destination" -type f -name "*.php" -exec sed -i "s/src='icons\//src='\/~jcd171\/icons\//g" {} +

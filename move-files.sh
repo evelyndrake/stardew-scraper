cfa6bff16b6666779426dc8d7ea189b27be865a0
evelyndrake <evelyndrake90@gmail.com>
2025-03-24 12:44:09 -0400

    Edited install script

--- move-files.sh
+++ move-files.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 destination="../cgi-bin"
 cp -r * "$destination/"
+#Add /~jcd171/ to all img tags
+find "$destination" -type f -name "*.php" -exec sed -i 's/src="images\//src="\/~jcd171\/images\//g' {} +

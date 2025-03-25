c0c9c1f2b9da4d0c4728a999e46230b7979e3242
evelyndrake <evelyndrake90@gmail.com>
2025-03-25 08:31:03 -0400

    Cleaned up install script

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ destination="../cgi-bin"
 cp -r * "$destination/"
 #Add /~jcd171/ to all img tags (src='icons/')
 find "$destination" -type f -name "*.php" -exec sed -i "s/src='icons\//src='\/~jcd171\/icons\//g" {} +
-#Add /~jcd171/ to all img tags (src="icons/")
+#Add /~jcd171/ to all img tags (src="icons/"), double quotes
 find "$destination" -type f -name "*.php" -exec sed -i 's/src=\"icons\//src=\"\/~jcd171\/icons\//g' {} +
-find "$destination" -type f -name "farming_tab.php" -exec sed -i 's/src=\"icons\//src=\"\/~jcd171\/icons\//g' {} +
 #Replace $image_path = "icons/..." with $image_path = "/~jcd171/icons/..."
 find "$destination" -type f -name "*.php" -exec sed -i 's,$image_path = "icons/,$image_path = "/~jcd171/icons/,g' {} +

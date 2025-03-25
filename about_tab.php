8bbb0dbfe45e683a43a3795acb87570dd363c8e7
evelyndrake <evelyndrake90@gmail.com>
2025-03-25 08:55:24 -0400

    Updated contact info on about page

--- about_tab.php
+++ about_tab.php
@@ -3,12 +3,14 @@ function displayAboutTab()
 {
     echo '<div id="about" class="tabcontent" style="display: none">
         <div class="controls">
-                <h4>About</h4>
-                Written by <a href="https://www.github.com/evelyndrake">Evelyn Drake</a>
-        for CSDS 285 (Linux Tools and Scripting).
-        <br/>
-        View the source code <a href="https://github.com/evelyndrake/stardew-scraper">here.</a>
-                <br/>
+            <h4>About</h4>
+            Written by <b>Evelyn Drake</b>
+            for CSDS 285 (Linux Tools and Scripting) at Case Western Reserve University.
+            <ul class="contact-links">
+                <li><a href="https://www.github.com/evelyndrake">Github</a></li>
+                <li><a href="https://evelynsethernet.dev">Personal website</a></li>
+            </ul>
+            View the source code <a href="https://github.com/evelyndrake/stardew-scraper">here.</a>
         </div>
     </div>';
     }
